011e7dd1f8f61d2fe79d7fce6a54f354877227a1
mailo17 <ivolkier@gmail.com>
2025-05-03 15:48:41 +0300

    F-1

---
+++ mc3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int int main(int argc, char *argv[]) {
+  int days;
+
+  int maxTries = 1;
+  int maxChanges = 10;
+  int numberofvariables = 5;
+  int days = 10;
+  int Xvalue[numberofvariables]; // X1, X2, ..., X70...values...Practically X1,
+                                 // X2, ..., X70
+  int numberofvalues = days * 3; // Timeslots = days * 3
+  int PrecedureRestarts = 2;     // The whole procedure restarts
+
+  return EXIT_SUCCESS;
+}
